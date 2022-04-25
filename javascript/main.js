e3aae9b26aa9c5195f293684dcb760863dcb8f32
Mathias231 <mathias.krogh@outlook.com>
2022-04-25 14:23:49 +0200

    UPDATED

--- javascript/main.js
+++ javascript/main.js
@@ -2,16 +2,18 @@ $.getJSON("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&
     
     console.log(data);
 
-    var currentWeather = data.properties.timeseries[2].data.instant.details;
-    console.log(currentWeather);
+    var currentWeatherData = data.properties.timeseries[2].data.instant.details;
+    console.log(currentWeatherData);
 
-    var currentTemp = currentWeather.air_temperature;
-    var currentWind = currentWeather.wind_speed;
+    var currentTemp = currentWeatherData.air_temperature.toFixed(0);
+    var currentWind = currentWeatherData.wind_speed.toFixed(1);
 
 
     $(document).ready(function(){
-        $("#tempNow").html("Temperaturen nå: " + currentTemp + "°C");
-        $("#windNow").html("Vindhastigheten nå: " + currentWind + "m/s");
+        $("#tempNow").html("Temperaturen nå: " + currentTemp + " °C");
+        $("#windNow").html("Vindhastigheten nå: " + currentWind + " m/s");
     });
 });
+
+
 // 2 is current time
