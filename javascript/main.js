661549ab3c33eaa8d0d22ad8f1647a1adc3e5190
Mathias231 <mathias.krogh@outlook.com>
2022-04-28 14:23:17 +0200

    UPDATED

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,11 @@ $.getJSON("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&
 
     // Current weather
     var currentWeatherData = data.properties.timeseries[2].data.instant.details;
-    console.log(currentWeatherData);
+    // console.log(currentWeatherData);
     
     // Weather next hour
     var weatherNextHour = data.properties.timeseries[3].data.instant.details;
-    console.log(weatherNextHour);
+    // console.log(weatherNextHour);
 
 
     $(document).ready(function(){
@@ -21,13 +21,24 @@ $.getJSON("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=67.28&
         $("#windNextHour").html("Vindhastigheten neste time: " + weatherNextHour.wind_speed.toFixed(1) + " m/s");
 
     
-        // Weather description
-        var weatherIcon = data.properties.timeseries[1].data.next_1_hours.summary.symbol_code;
-        console.log(weatherIcon);
-
-        var svgPath = "../svg/" + weatherIcon + ".svg";
-        
+        // Weather description for current weather
+        // Grabbing weather symbol from timeseries[1] (wich is the previous hour) and uses it's "next_1_hours" to get the weather icon
+        var weatherIconCurrent = data.properties.timeseries[1].data.next_1_hours.summary.symbol_code;
+        // Set path to weather icon
+        var svgPath = "../svg/" + weatherIconCurrent + ".svg";
+        // Set weather icon
         $("#weatherIcon").attr("src", svgPath);
+        //console.log(weatherIconCurrent);
+
+        // Weather description
+        // Grabbing weather symbol from timeseries[2] and uses it's "next_1_hours" to get the weather icon to predict the weather for the next hour
+        var weatherIconNextHour = data.properties.timeseries[2].data.next_1_hours.summary.symbol_code;
+        // Set path to weather icon
+        var svgPath = "../svg/" + weatherIconNextHour + ".svg";
+        // Set weather icon
+        $("#weatherIconNextHour").attr("src", svgPath);
+        
+        
     });
 });
 
